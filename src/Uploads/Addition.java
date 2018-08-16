a1c7d34d8cce39519118f55a332b4c05de291eab
nicky <aditirkb@gmail.com>
2018-08-16 12:26:39 +0530

    first

---
+++ src/Uploads/Addition.java
@@ -0,0 +1,25 @@
+package Uploads;
+
+import java.util.Scanner;
+
+public class Addition {
+	
+	
+			public static void main(String[] args) {
+				
+				Scanner sc=new Scanner(System.in);
+				System.out.println("enter the value of the A");
+				
+				
+				int a= sc.nextInt();
+				System.out.println("enter the value of the B");
+				int b=sc.nextInt();
+				
+				int sum=a+b;
+				System.out.println("value is :" +sum);
+				
+				
+			}
+	
+
+}
